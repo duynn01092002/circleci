6f04b6d098148ded79ee72f62381a0492c7df8f7
duynn01092002 <nguyenngocduy12a2@gmail.com>
2023-03-06 15:47:16 +0700

    add code

---
+++ test_math.py
@@ -0,0 +1,21 @@
+from math import sqrt
+
+def check_prime_number(x):
+    if x < 2:
+        return False
+    for i in range(2, x, 1):
+        if x % i == 0:
+            return False
+    return True
+
+def check_square_number(x):
+    y = int(sqrt(x))
+    if y**2 == x:
+        return True
+    return False
+
+def test_prime_number():
+    assert check_prime_number(5) == True
+
+def test_square_number():
+    assert check_square_number(5) == True
